f7a3100f604acd02f75801050154dda609191dc3
Denis <denis97@wp.pl>
2023-10-29 13:43:14 +0100

    resultbox test

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -1,31 +1,48 @@
-import PropTypes from 'prop-types';
-import { convertUSDToPLN } from './../../utils/convertUSDToPLN';
-import { convertPLNToUSD } from './../../utils/convertPLNToUSD';
-import { formatAmountInCurrency } from './../../utils/formatAmountInCurrency';
-import { useMemo } from 'react';
-import styles from './ResultBox.module.scss';
+import ResultBox from "./ResultBox";
+import { render, screen, cleanup } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
 
-const ResultBox = ({ from, to, amount }) => {
-
-  const convertedAmount = useMemo(() => {
-    if(from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
-    if(from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
-    return formatAmountInCurrency(amount, from);
-  }, [from, to, amount]);
-
-  const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
-
-  return (
-    <div className={styles.result}>
-      {formattedAmount} = {convertedAmount}
-    </div>
-  );
-};
-
-ResultBox.propTypes = {
-  from: PropTypes.string.isRequired,
-  to: PropTypes.string.isRequired,
-  amount: PropTypes.number.isRequired,
-}
-
-export default ResultBox;
+describe('Component ResultBox', () => {
+  it('should render withour crashing', () => {
+    render(<ResultBox from="PLN" to="USD" amount={100} />)
+  })
+  it('should render proper info about conversion when PLN -> USD”', () => {
+    const testCases = [
+      { amount: 100, usd: 28.57 },
+      { amount: 20, usd: 5.71 },
+      { amount: 209, usd: 59.71 }
+    ];
+    for (const test of testCases) {
+      render(<ResultBox from="PLN" to="USD" amount={test.amount} />)
+      const output = screen.getByTestId('mainDiv');
+      expect(output).toHaveTextContent(`PLN ${test.amount}.00 = $${test.usd}`);
+      cleanup();
+    }
+  });
+  it('should render proper info about conversion when PLN -> PLN”', () => {
+    const testCases = [
+      { amount: 100 },
+      { amount: 20 },
+      { amount: 209 }
+    ];
+    for (const test of testCases) {
+      render(<ResultBox from="PLN" to="PLN" amount={test.amount} />)
+      const output = screen.getByTestId('mainDiv');
+      expect(output).toHaveTextContent(`PLN ${test.amount}.00 = PLN ${test.amount}.00`);
+      cleanup();
+    }
+  });
+  it('should render proper info about conversion, when amount below 0 show "Wrong value”', () => {
+    const testCases = [
+      { amount: -100 },
+      { amount: -20 },
+      { amount: -209 }
+    ];
+    for (const test of testCases) {
+      render(<ResultBox from="PLN" to="PLN" amount={test.amount} />)
+      const output = screen.getByTestId('mainDiv');
+      expect(output).toHaveTextContent(`Wrong value`);
+      cleanup();
+    }
+  });
+});
---
+++ src/components/ResultBox/ResultBox.test.js
@@ -0,0 +1,6 @@
+import ResultBox from './ResultBox';
+  describe('Component ResultBox', () => {
+});
+it('should render without crashing', () => {
+    render(<ResultBox from="PLN" to="USD" amount={100} />);
+});
